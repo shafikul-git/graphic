24c1fdba880dd41c24138614210ba848bf8f3c71
shafikul <shafikul.18288@gmail.com>
2024-11-07 07:49:02 +0600

    file upload

--- app/Http/Controllers/setting/FileUploaderController.php
+++ app/Http/Controllers/setting/FileUploaderController.php
@@ -2,17 +2,43 @@
 
 namespace App\Http\Controllers\setting;
 
-use App\Http\Controllers\Controller;
+use DateTimeZone;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class FileUploaderController extends Controller
 {
     public function test(){
         return view('dashboard.setting.cehck');
     }
-    public function store(Request $request){
-        return response()->json([
-            $request
-        ]);
+    public function store(Request $request)
+    {
+        $getDate = new \DateTime("now", new DateTimeZone("Asia/Dhaka"));
+        $currentDate = $getDate->format('Y_m_d H-i-s');
+        $storeFileNam = [];
+
+        if ($request->hasFile('file')) {
+            // Handle single or multiple file uploads
+            $files = $request->file('file');
+
+            // If multiple files, loop through them
+            if (is_array($files)) {
+                foreach ($files as $file) {
+                    $changeName = $currentDate . '_' . $file->getClientOriginalName();
+                    $path = $file->move(public_path('uploads'), $changeName);
+                    $storeFileNam[] = $path;
+                }
+            } else {
+                // For single file upload
+                $changeName = $currentDate . '.' . $files->getClientOriginalExtension();
+                $path = $files->move(public_path('uploads'), $changeName);
+                $storeFileNam[] = $path;
+            }
+
+            return response()->json(['success' => 'Files uploaded successfully', 'paths' => $storeFileNam]);
+        }
+
+        return response()->json(['error' => 'No files uploaded'], 400);
     }
+
 }
--- resources/views/dashboard/setting/cehck.blade.php
+++ resources/views/dashboard/setting/cehck.blade.php
@@ -2,61 +2,57 @@
 @extends('layouts.contentNavbarLayout')
 @section('content')
 
-<x-form method="POST" action="{{ route('test') }}" enctype="multipart/form-data" id="formData">
-    <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="file" name="file" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div class="progress mt-4" role="progressbar" aria-label="Basic example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-            <div class="progress-bar" id="progressBar" style="width: 0%"></div>
-        </div>
+
+<div class="container mt-5">
+    <form action="{{ route('test') }}" class="dropzone" id="file-dropzone" enctype="multipart/form-data" method="POST">
+        @csrf
+    </form>
+    <div class="progress mt-4">
+        <div class="progress-bar" id="progressBar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </x-form>
+</div>
+
+
 @endsection
 
 
-{{-- @push('heads')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/min/dropzone.min.css" integrity="sha512-MCpE5+WLUt1ZP1OnGQx8HpUpxBlE7RjzXfwVVxRqcyFDRnTL0LGy3+JGGYXcmHSdFt06HREpQ9euEd0jAzdoQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-@endpush --}}
+@push('heads')
+<script src="https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone-min.js"></script>
+<link href="https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone.css" rel="stylesheet" type="text/css" />
+@endpush
 
 
 @push('scripts')
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/min/dropzone.min.js" integrity="sha512-pDZyQVDHJ4crfBcI1HdBiB9PeL5Nuj2FDiI05Xab7B8Gszcx+vn5/08KH3i2BimOpyu1mfj4VLHbV+kGKb27PA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <script type="text/javascript">
-   $(document).ready(function(){
-    $('#formData').on('submit', function(event){
-        event.preventDefault();
-
-        var formData = new FormData(this);
-        $.ajax({
-            xhr: function() {
-                var xhr = new window.XMLHttpRequest();
-                xhr.upload.addEventListener("progress", function(event) {
-                    if (event.lengthComputable) {
-                        var percentComplete = (event.loaded / event.total) * 100;
-                        $('#progressBar').css('width', percentComplete + '%');
-                        $('#progressBar').attr('aria-valuenow', percentComplete);
-                    }
-                }, false);
-                return xhr;
-            },
-            url: "{{ route('test') }}",
-            type: "POST",
-            data: formData,
-            contentType: false,
-            processData: false,
-            success: function(response){
-                alert('File uploaded successfully');
-            },
-            error: function(response){
-                alert('An error occurred while uploading the file');
-            }
+    var dropzone = new Dropzone("#file-dropzone", {
+    parallelUploads: 10,
+    uploadMultiple: true,
+    addRemoveLinks: true,
+    init: function() {
+        this.on("sending", function(file, xhr, formData) {
+            formData.append("_token", "{{ csrf_token() }}");
         });
-    });
-   });
+
+        this.on("totaluploadprogress", function(progress) {
+            $('#progressBar').css('width', progress + '%');
+            $('#progressBar').attr('aria-valuenow', progress);
+        });
+
+        this.on("queuecomplete", function() {
+            $('#progressBar').css('width', '0%');
+        });
+
+        this.on("success", function(file, response) {
+            console.log('File uploaded successfully');
+        });
+
+        this.on("error", function(file, response) {
+            console.log('Error uploading file');
+        });
+    }
+});
+
 </script>
+
 @endpush
